8af2d06c4a2547679db9324ece110ceba286fb73
Rajendra1103 <166483824+Rajendra1103@users.noreply.github.com>
2025-01-31 10:47:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const passwordField = document.getElementById("password");
+
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const includeUppercase = document.getElementById("uppercase").checked;
+    const includeLowercase = document.getElementById("lowercase").checked;
+    const includeNumbers = document.getElementById("numbers").checked;
+    const includeSymbols = document.getElementById("symbols").checked;
+
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+-=[]{}|;:'\",.<>?/";
+
+    let charPool = "";
+    if (includeUppercase) charPool += uppercaseChars;
+    if (includeLowercase) charPool += lowercaseChars;
+    if (includeNumbers) charPool += numberChars;
+    if (includeSymbols) charPool += symbolChars;
+
+    if (charPool === "") {
+        passwordField.value = "Select at least one option!";
+        return;
+    }
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charPool.length);
+        password += charPool[randomIndex];
+    }
+
+    passwordField.value = password;
+}
+
+function copyPassword() {
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
+}
